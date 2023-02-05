b4416db51e9f414ba4344313204887b02f3bdb1b
Deepsri1923 <124479271+Deepsri1923@users.noreply.github.com>
2023-02-05 14:11:48 +1100

    Add files via upload
    Day1

---
+++ day1/HelloWorld.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("testing");
+		boolean b = true;
+		Integer a = 123;
+		String name = "Deepa";
+		System.out.println("Integer is " + a + "\n" + "Boolean value is  " + b + "\n" + " String name is " + name);
+
+	}
+}
